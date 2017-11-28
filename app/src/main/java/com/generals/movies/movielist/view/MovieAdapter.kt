0dbb5ed57c61dbf61f01a731ce9c10fb2bc4125b
generalimattia <mattigene@gmail.com>
2017-11-28 19:35:47 +0100

    Add progress bar when downloading image from url

--- app/src/main/java/com/generals/movies/movielist/view/MovieAdapter.kt
+++ app/src/main/java/com/generals/movies/movielist/view/MovieAdapter.kt
@@ -1,11 +1,16 @@
 package com.generals.movies.movielist.view
 
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.generals.movies.R
 import com.generals.movies.movielist.model.Movie
 import kotlinx.android.synthetic.main.widget_movie_item.view.*
@@ -30,10 +35,28 @@ class MovieAdapter(val movieList: List<Movie>) : RecyclerView.Adapter<MovieAdapt
 
     class MovieViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(movie: Movie) {
-            Glide.with(itemView.context)
-                    .load(movie.imageUrl)
-                    .apply(RequestOptions.centerCropTransform())
-                    .into(itemView.movie_image)
+            itemView.image_loader.visibility = View.VISIBLE
+            itemView.movie_image.contentDescription = movie.title
+
+            Glide.with(itemView.context).load(movie.imageUrl).listener(object : RequestListener<Drawable> {
+
+                override fun onResourceReady(resource: Drawable?,
+                                             model: Any?,
+                                             target: Target<Drawable>?,
+                                             dataSource: DataSource?,
+                                             isFirstResource: Boolean): Boolean {
+                    itemView.image_loader.visibility = View.GONE
+                    return false
+                }
+
+                override fun onLoadFailed(e: GlideException?,
+                                          model: Any?,
+                                          target: Target<Drawable>?,
+                                          isFirstResource: Boolean): Boolean {
+                    itemView.image_loader.visibility = View.GONE
+                    return false
+                }
+            }).apply(RequestOptions.centerCropTransform()).into(itemView.movie_image)
         }
     }
 
